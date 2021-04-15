f870d87867f0c4a7b52a6ca0b15e47e8ca71f0dd
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-15 21:26:02 +0200

    Fixed symbols function returning epsilons

--- src/Automata/Automata.hs
+++ src/Automata/Automata.hs
@@ -1,21 +1,23 @@
 module Automata.Automata where
 
-import Automata.Types
-import Data.List
+import Automata.Types 
+import Data.List (nub)
 
 states :: Automata -> [State]
 states a = nub $ origins ++ dests
   where origins = origin <$> transitions a
         dests   = dest   <$> transitions a
 
+-- Returns all symbols (no epsilon)
 symbols :: Automata -> [Symbol]
-symbols = nub . map symbol . transitions
+symbols = filter (/= Epsilon) . nub . map symbol . transitions
 
 stackSymbols :: Automata -> [StackSymbol]
 stackSymbols a = nub $ newTops ++ tops
   where tops    = stackTop <$> transitions a
         newTops = transitions a >>= newStackTop
 
+-- This is the δ(q, a, X) function
 deltaTransitions :: Automata -> State -> Symbol -> StackSymbol -> [Transition]
 deltaTransitions a orig sym stTop = 
   filter (\(Transition {origin, symbol, stackTop}) -> 
