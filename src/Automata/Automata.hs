04b209a4384bd0fac1102046ce5a28569b06c54d
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-15 20:56:38 +0200

    Added important functions for Automata

--- src/Automata/Automata.hs
+++ src/Automata/Automata.hs
@@ -1,2 +1,25 @@
 module Automata.Automata where
 
+import Automata.Types
+import Data.List
+
+states :: Automata -> [State]
+states a = nub $ origins ++ dests
+  where origins = origin <$> transitions a
+        dests   = dest   <$> transitions a
+
+symbols :: Automata -> [Symbol]
+symbols = nub . map symbol . transitions
+
+stackSymbols :: Automata -> [StackSymbol]
+stackSymbols a = nub $ newTops ++ tops
+  where tops    = stackTop <$> transitions a
+        newTops = transitions a >>= newStackTop
+
+deltaTransitions :: Automata -> State -> Symbol -> StackSymbol -> [Transition]
+deltaTransitions a orig sym stTop = 
+  filter (\(Transition {origin, symbol, stackTop}) -> 
+              origin   == orig &&
+              symbol   == sym  &&
+              stackTop == stTop
+         ) $ transitions a
