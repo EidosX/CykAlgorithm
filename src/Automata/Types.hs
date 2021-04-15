1a741733942d711a73fd23df0376e371d98d9d74
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-15 20:03:52 +0200

    Wrote Automata Show instance

--- src/Automata/Types.hs
+++ src/Automata/Types.hs
@@ -1,5 +1,7 @@
 module Automata.Types where
 
+import Data.List (intercalate)
+
 data State = State String
   deriving Eq
 
@@ -21,3 +23,20 @@ data Automata = Automata {
   transitions :: [Transition],
   finalStates :: [State]
 }
+
+-- For debug purposes
+instance Show Symbol where show (Symbol s) = s
+                           show Epsilon    = "ε"
+
+instance Show Transition where
+  show t = o <> " -> " <> d <> " (" <> s <> ", " <> st <> "/" <> nst <> ")"
+    where State o        = origin t
+          State d        = dest   t
+          s              = show $ symbol t
+          StackSymbol st = stackTop t
+          nst            = intercalate "." . map (\(StackSymbol a) -> a) $ newStackTop t
+
+instance Show Automata where
+  show Automata {transitions, finalStates} =
+    "Transitions: \n" <> unlines (("  " ++) . show <$> transitions) <>
+    "Final States: " <> intercalate ", " ((\(State a) -> a) <$> finalStates)
