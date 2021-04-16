6094efebe993e07be53fcd1fe6bdfbc018f9c648
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-16 19:08:27 +0200

    removeNonSolitaryTerminals function

--- src/Grammar/CNFMaker.hs
+++ src/Grammar/CNFMaker.hs
@@ -2,11 +2,12 @@ module Grammar.CNFMaker (makeCNF) where
 
 import Grammar.Types
 import Grammar.Grammar
+import Data.Either (partitionEithers)
 
 makeCNF :: Grammar -> Grammar
 makeCNF = undefined
         . undefined
-        . undefined
+        . removeLongRHS
         . removeNonSolitaryTerminals
         . removeEntrySymbolFromRHS
 
@@ -19,8 +20,16 @@ removeEntrySymbolFromRHS g = Grammar (newTrans : rules g) newEntry
 removeNonSolitaryTerminals :: Grammar -> Grammar
 removeNonSolitaryTerminals g' = g' {rules = rules g' >>= f}
   where f :: Rule -> [Rule]
-        f t | length (to t) <= 1 = [t]
-            -- | otherwise = -- TODO
+        f r | length (to r) <= 1 = [r]
+        f r = r {to = Left <$> rVars ++ terminalVars} : terminalRules
+          where
+            terminalToVar (Terminal sym) = createNewVar (vars g') $ "X" ++ sym
+            terminalVars = map terminalToVar rTerminals
+            terminalRules = map (\t -> Rule (terminalToVar t) [Right t]) rTerminals
+            (rVars, rTerminals) = partitionEithers $ to r
+
+removeLongRHS :: Grammar -> Grammar
+removeLongRHS g' = undefined
 
 -- Makes sure not to have name conflict
 createNewVar :: [Var] -> String -> Var
