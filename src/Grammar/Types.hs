b1f9ea2399c3c4c89d17c661912050573ac6c98e
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-16 19:07:14 +0200

    Debugging Show instance

--- src/Grammar/Types.hs
+++ src/Grammar/Types.hs
@@ -15,3 +15,14 @@ data Grammar = Grammar {
   rules    :: [Rule],
   entryVar :: Var
 }
+
+-- For debugging
+
+instance Show Rule where
+  show (Rule (Var from) ts) = from ++ " -> " ++ unwords (map showSym ts)
+    where showSym (Right x) = "(" ++ show x ++ ")"
+          showSym (Left x) = "(" ++ show x ++ ")"
+
+instance Show Grammar where
+  show Grammar {rules, entryVar} = "Entry: " ++ show entryVar ++ "\nRules:\n" ++
+    unlines (map (("  " ++) . show) rules)
