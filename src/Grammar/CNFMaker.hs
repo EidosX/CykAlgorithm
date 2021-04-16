f4a7fc13807ad083a081a61a9921800cdf089196
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-16 21:05:07 +0200

    function to remove long right hand sides (length > 2)

--- src/Grammar/CNFMaker.hs
+++ src/Grammar/CNFMaker.hs
@@ -17,16 +17,24 @@ removeEntrySymbolFromRHS g = Grammar (newTrans : rules g) newEntry
         newEntry = createNewVar (vars g) oldEntryStr
 
 removeNonSolitaryTerminals :: Grammar -> Grammar
-removeNonSolitaryTerminals g' = g' {rules = terminalRules ++ map f (rules g')}
+removeNonSolitaryTerminals g = g {rules = terminalRules ++ map f (rules g)}
   where f r | length (to r) <= 1 = r
             | otherwise          = r {to = terminalReplacer <$> to r}
-        terminalRules = (\t -> Rule (terminalToVar t) [Right t]) <$> terminals g'
+        terminalRules = (\t -> Rule (terminalToVar t) [Right t]) <$> terminals g
         terminalReplacer (Left v)  = Left v
         terminalReplacer (Right t) = Left $ terminalToVar t
-        terminalToVar (Terminal t) = createNewVar (vars g') ("X_" ++ t)
+        terminalToVar (Terminal t) = createNewVar (vars g) ("X_" ++ t)
 
+-- This function pre-supposes vars and terminals are separated in each rule
 removeLongRHS :: Grammar -> Grammar
-removeLongRHS _g' = undefined
+removeLongRHS g' | all (<= 2) (length . to <$> rules g') = g'
+                 | otherwise = removeLongRHS g' {rules = f $ rules g'} where
+  f [] = []
+  f (r:rs) | length (to r) > 2 = headRule : newRule : rs
+           | otherwise = r : f rs
+    where newVar = createNewVar (vars g') "Q"
+          newRule = Rule newVar (tail $ to r)
+          headRule = Rule (from r) [head $ to r, Left newVar]
 
 -- Makes sure not to have name conflict
 createNewVar :: [Var] -> String -> Var
