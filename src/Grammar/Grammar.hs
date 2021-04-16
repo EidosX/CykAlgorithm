1b01d71b00a7f30310bcac41f1f9fcf67181af7d
Diego Imbert <imbert.diego05000@gmail.com>
2021-04-16 14:43:51 +0200

    Grammar isCNF and other functions

---
+++ src/Grammar/Grammar.hs
@@ -0,0 +1,20 @@
+module Grammar.Grammar where
+
+import Grammar.Types
+import Data.List (nub)
+import Data.Either (lefts, rights)
+
+isCNF :: Grammar -> Bool
+isCNF Grammar {transitions, entryVariable} = all valid transitions where 
+  valid (Transition _ [Right _]        )                                   = True
+  valid (Transition _ [Left v, Left v']) | entryVariable `notElem` [v, v'] = True
+  valid (Transition v []               ) | v == entryVariable              = True
+  valid (Transition _ _                )                                   = False
+
+variables :: Grammar -> [Variable]
+variables g = nub $ entryVariable g 
+                  : map from (transitions g) 
+                  ++ (transitions g >>= lefts . to)
+
+terminals :: Grammar -> [Terminal]
+terminals g = transitions g >>= rights . to
