f8236127985a488845e5054e966ecaa4b2de1742
Diego Imbert <imbert.diego05000@gmail.com>
2021-05-04 19:32:56 +0200

    Simplified CYK function

--- src/Grammar/CYK.hs
+++ src/Grammar/CYK.hs
@@ -4,20 +4,18 @@ import Grammar.Types
 import Data.Function.Memoize (memoize2)
 import Data.List (nub)
 
+-- This function assumes all terminals in grammar are a single char
 cykStr :: Grammar -> [Char] -> Bool
 cykStr g = cyk g . map (\c -> Terminal [c])
 
 -- This function assumes a grammar in chomsky normal form
 cyk :: Grammar -> [Terminal] -> Bool
 cyk g [] = any ((== []) . to) $ rules g
-cyk g str = entryVar g `elem` cyk' g str (length str - 1) 0
-
-cyk' :: Grammar -> [Terminal] -> Int -> Int -> [Var]
-cyk' g str = memoized where 
-  memoized = memoize2 f
+cyk g str = entryVar g `elem` cyk' (length str - 1) 0 where
+  cyk' = memoize2 f
   f j i | j == 0 = nub . map from . filter ((== [Right $ str !! i]) . to) $ rules g
         | otherwise = do
             splitIndex <- [1..j]
-            leftVar <- memoized (splitIndex - 1) i
-            rightVar <- memoized (j - splitIndex) (i + splitIndex)
+            leftVar <- cyk' (splitIndex - 1) i
+            rightVar <- cyk' (j - splitIndex) (i + splitIndex)
             map from . filter ((== [Left leftVar, Left rightVar]) . to) $ rules g
